4fa32d857c90c29f8985b50f42bee1844f84ed71
FajarRizkycode <fajar.andiansyah24@student.uisi.ac.id>
2026-05-07 13:03:27 +0700

    add tanggalTask

---
+++ script.js
@@ -0,0 +1,33 @@
+const inputValue = document.getElementById("inputTask");
+const btnTambah = document.getElementById("btnTambahTodo");
+const daftartugas = document.getElementById("daftartugas");
+
+btnTambah.addEventListener("click", function(){
+
+    if(inputValue.value === ""){
+        alert("Input tidak boleh kosong");
+        return;
+    }
+
+    const listbaru = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerHTML = inputValue.value;
+
+    listbaru.appendChild(span);
+    daftartugas.appendChild(listbaru);
+
+    inputValue.value = "";
+    inputValue.focus();
+
+
+
+
+
+
+
+
+
+
+
+});
+
