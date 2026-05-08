a307f7087782f2fe75d1e477f409b9b5f19fec59
FajarRizkycode <fajar.andiansyah24@student.uisi.ac.id>
2026-05-08 13:27:45 +0700

    add perbaikan js

--- script.js
+++ script.js
@@ -1,65 +1,52 @@
 const inputValue = document.getElementById("inputTask");
+const tanggalTask = document.getElementById("tanggalTask");
 const btnTambah = document.getElementById("btnTambahTodo");
 const daftartugas = document.getElementById("daftartugas");
 
 btnTambah.addEventListener("click", function(){
 
-    if(inputValue.value === ""||
-       tanggalTask.value === ""
+    if(
+        inputValue.value === "" ||
+        tanggalTask.value === ""
     ){
-        alert("Input tidak boleh kosong");
-        alert("Tanggal tidak boleh kosong");
+        alert("Input dan tanggal tidak boleh kosong");
         return;
     }
 
     const listbaru = document.createElement("li");
+
     const span = document.createElement("span");
     span.innerHTML = inputValue.value;
 
-    const tanggal = document.getElementById("tanggaTask").value;
-    tanggal.innerHTML = "Tanggal"+tanggal;
+    const tanggal = document.createElement("small");
+    tanggal.innerHTML = "Tanggal: " + tanggalTask.value;
 
     listbaru.appendChild(span);
     listbaru.appendChild(tanggal);
-    
+
+    const btnEdit = document.createElement("button");
+    btnEdit.innerHTML = "Edit";
+    btnEdit.classList.add("edit");
+
+    listbaru.appendChild(btnEdit);
+
+    btnEdit.addEventListener("click", function(){
+
+        const editTask = prompt(
+            "Edit tugas:",
+            span.innerHTML
+        );
+
+        if(editTask !== null && editTask !== ""){
+            span.innerHTML = editTask;
+        }
+
+    });
+
     daftartugas.appendChild(listbaru);
 
     inputValue.value = "";
     tanggalTask.value = "";
     inputValue.focus();
 
-    const btnEdit = document.createElement("button");
-    btnEdit.innerHTML = "Edit";
-
-    listbaru.appendChild("butnEdit");
-
-    btnEdit.addEventListener("click", function(){
-
-    const editTask = prompt(
-        "Edit tugas:",
-        span.innerHTML
-    );
-
-    if(
-        editTask !== null &&
-        editTask !== ""
-    ){
-        span.innerHTML = editTask;
-    }
-
 });
-
-btnEdit.classList.add("edit");
-
-
-
-
-
-
-
-
-
-
-
-});
-
